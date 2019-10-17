fcc830708ccaea8f11f65c7c768713b66c6a7ba3
MrBradabie <55830472+MrBradabie@users.noreply.github.com>
2019-10-17 20:15:00 +0700

    Add files via upload

---
+++ IF1-10118006-Latihan40-(ObjectOriented)WarnaKepribadian/src/if1/pkg10118006/latihan40/objectoriented/warnakepribadian/IF110118006Latihan40ObjectOrientedWarnaKepribadian.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118006.latihan40.objectoriented.warnakepribadian;
+
+/**
+ *
+ * @author 
+ * NAMA     : Tias Gumelar Timorisky
+ * KELAS    : IF-3
+ * NIM      : 10117111
+ * Deskripsi Program : Program ini untuk menampilkan tampilan dari warna dan 
+ *                     kepribadian.
+ */
+public class IF110118006Latihan40ObjectOrientedWarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Warna skuyMu = new Warna();
+        
+        skuyMu.ioWarnaMu();
+    }
+    
+}
